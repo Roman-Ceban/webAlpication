0f5b602b3cef3239fc214e8ed9366e0c79a9e3a3
Roman-Ceban <xayshondet18@gmail.com>
2022-12-13 12:43:06 +0200

    First Commit
    First Commit

---
+++ webAplication/webAplication/src/test/java/md/Roman/webAplication/WebAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package md.Roman.webAplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
